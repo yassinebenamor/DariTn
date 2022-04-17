8c0daa4385d8fc9fa7fc20943ddb6e647ba019f1
unknown <heni.guirat@esprit.tn>
2022-04-17 02:24:27 +0100

    Premiere commit

--- src/main/java/Daritn/spring/repository/AnnonceRepository.java
+++ src/main/java/Daritn/spring/repository/AnnonceRepository.java
@@ -1,7 +1,10 @@
 package Daritn.spring.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import Daritn.spring.entity.Annonce;
+
 @Repository
-public interface AnnonceRepository {
+public interface AnnonceRepository extends JpaRepository<Annonce, Long>{
 
 }
